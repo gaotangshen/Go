bc9f9139f433bba0ca7827a02cb3f2a7a79d3541
Denzel <denzel@Denzel-Ubuntu.(none)>
2015-08-04 18:38:07 -0400

    gt commit

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+	// "io"
+	// "log"
+	// "net/http"
+	"github.com/astaxie/beego"
+	_ "hello/routers"
 )
+
+// type MainController struct {
+// 	beego.Controller
+// }
 
 func main() {
 	// Router
-	http.HandleFunc("/", sayHello)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// beego.Router("/", &MainController{})
+	beego.Run()
+	// http.HandleFunc("/", sayHello)
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello. world")
-}
+// func sayHello(w http.ResponseWriter, r *http.Request) {
+// 	io.WriteString(w, "Hello. world")
+// }
